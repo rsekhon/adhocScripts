87763388d9c0880d58d6d44d425b1b934131d080
Ravinder Sekhon <rsekhon@me.com>
2020-11-13 17:12:56 -0800

    learning python

---
+++ Python/learning.py
@@ -0,0 +1,9 @@
+# Python Full Course from You Tube freecode.org
+
+nam = input('Who are you? ')
+
+euf = input('Europe Floor? ')
+usf = int(euf) + 1
+
+print ('Well ', nam, ', Europe floor is ', euf, ' and US floor is ', usf)
+
